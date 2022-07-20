de8ee8fb1d1d3394bc609fd96fb0248344658ae4
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-07-20 12:12:30 +0200

    first image with dynamic props is rendered commit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {IProduct} from "../../models/product";
 
 @Component({
   selector:'app-product',
@@ -6,5 +7,5 @@ import {Component} from "@angular/core";
 })
 
 export class ProductComponent{
-
+@Input() product:IProduct
 }
