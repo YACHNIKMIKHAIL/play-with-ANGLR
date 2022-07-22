d06684a96514ec4cac8c90196f646c03392b959d
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-07-22 12:50:44 +0200

    add routing commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProductPageComponent} from "./pages/product-page/product-page.component";
+import {AboutPageComponent} from "./pages/about-page/about-page.component";
 
-const routes: Routes = [];
+enum RoutesEnum {
+  product = '',
+  about = 'about'
+}
+
+const routes: Routes = [
+  {path: RoutesEnum.product, component: ProductPageComponent},
+  {path: RoutesEnum.about, component: AboutPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {IProduct} from "./models/product";
-import {ProductsService} from "./services/products.service";
-import {Observable, tap} from "rxjs";
-import {ModalService} from "./services/modal.service";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'my-angular-app';
-  products: IProduct[] = []
-  loading  = false
-  // products$: Observable<IProduct[]>
-  term = ''
-
-  constructor(public productsService: ProductsService,
-              public modalService: ModalService) {
-  }
-
-  ngOnInit(): void {
-    this.loading = true
-    // this.products$ = this.productsService.getAll().pipe(tap(() => this.loading = false))
-    this.productsService.getAll().subscribe(() => {
-      this.loading = false
-    })
-  }
+export class AppComponent {
 }
